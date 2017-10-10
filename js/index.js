87b51082f8193727bb2cd75b9196b8456af95dcd
Andrea Baker <ambaker.888@gmail.com>
2017-10-10 15:32:21 -0400

    add jekyll and touch up index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-var currentRoute = '';
-
 function loadNav () {
   $('#nav').load('components/nav.html');
 }
@@ -8,21 +6,30 @@ function loadFooter () {
   $('#footer').load('components/footer.html');
 }
 
-function gotoRoute (route, anchor) {
-  if (route != currentRoute) {
-    currentRoute = route;
-    $('#content').load('routes/' + route, function () {
-      window.location.href = '#' + (anchor || '');
+function gotoRoute (route) {
+  sessionStorage.setItem('currentRoute', route);
 
-      if (!anchor) {
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
-      }
-    });
-  }
+  let [ page, anchor ] = route.split('#');
+
+  $('#content').load('routes/' + page, function () {
+    // window.location.href = '#' + (anchor || '');
+
+    window.location.hash = '#' + anchor;
+    // let element = $('#' + anchor)
+    // let scrollTop = element.length > 0 ? element.offset().top + $('#content').scrollTop() : 0;
+    // document.body.scrollTop = document.documentElement.scrollTop = scrollTop;
+    // scrollTo(document.body, scrollTop, 100);
+  });
 }
 
 function init () {
   loadNav();
   loadFooter();
-  gotoRoute('home.html');
+
+  let currentRoute = sessionStorage.getItem('currentRoute');
+  gotoRoute([ undefined, 'undefined', null, 'null' ].includes(currentRoute) ? 'home.html' : currentRoute);
 }
+
+$(window).load(function () {
+  init();
+});
